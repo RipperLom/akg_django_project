d1c8e4bce4cd374afca80271aa906f2dc6656322
zhaoguanzhi <zhaoguanzhi1992@163.com>
2019-05-29 10:08:30 +0800

    zgz

--- akg_django_project/urls.py
+++ akg_django_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from akg_django_project import index_view, index_ERform_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', index_view.index),
+    url(r'^ER-post', index_ERform_view.ER_post),
 ]
